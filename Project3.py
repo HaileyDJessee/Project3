d62f9375bd6a7dcb516d427a0ab65f91eb0d7d4b
JusticeGtrrz <111757662+JusticeGtrrz@users.noreply.github.com>
2022-10-25 17:56:53 -0500

    Update Project3.py

--- Project3.py
+++ Project3.py
@@ -1,76 +1,107 @@
 import requests
 import pygal
-import lxml
-StockSymbol =""
-TimeSeries = 0
+from lxml import html
+
+ChartChoice = 0
+ChartType = ["bar_chart", "line_chart"]
+DateChoice = ""
+Date = '&date=' + DateChoice
 Function = ['function=TIME_SERIES_DAILY','function=TIME_SERIES_WEEKLY','function=TIME_SERIES_MONTHLY','function=TIME_SERIES_INTRADAY']
-Date = '&date=2022-06-09'
-def StockMod():
-    StockData = "y"
+StockData = "y"
+StockSymbol = ""
+Symbol = ""
+TimeSeries = 0
+
+def StockFunc():
+    global Symbol
     print("Stock Data Visualizer")
     print("-----------------------")
     while StockData != "n":
         if StockData !="y" and StockData !="n":
             print("Error Data Type Invalid. Please enter 'y' or 'n'")
         else:
-            try: 
-                StockSymbol = input("Enter the stock symbol you are looking for: ")
-                if StockSymbol == "":
-                    print("Error Data Type Invalid")
-                else:
-                    Symbol = '&symbol='+ StockSymbol
-                    print("Chart Types")
-                    print("-----------------------")
-                    print("1. Bar")
-                    print("2. Line")
-                    ChartType = int(input("Enter the chart type you want (1, 2):"))
-                    if ChartType == 1:
-                        print("you have chosen Bar Chart")
-                        print("Select the Time Series of the chart you want to Generate")
-                        print("-----------------------")
-                        print("1. Daily")
-                        print("2. Weekly")
-                        print("3. Monthly")
-                        print("4. Intraday")
-                        TimeSeries = int(input("Enter time series option (1, 2, 3, 4):"))
-                        if TimeSeries != 1 and TimeSeries != 2 and TimeSeries != 3 and TimeSeries != 4:
-                            print("Error Data Type Invalid")
-                        else:
-                            URL = 'https://www.alphavantage.co/query?'+ Function[TimeSeries-1] +'&date=2022-06-09'+ Symbol +'&apikey=7CKUYMD19R6Q9LKW'
-                            print(URL)
-                            StartDate = input("Enter the start date (YYYY-MM-DD):")
-                            EndDate = input("Enter the end date (YYYY-MM-DD):")
-                            r = requests.get(URL)
-                            data = r.json()
-                            print(data)
-                            #bar_chart = pygal.Bar
-                            #bar_chart.add(str(data))
-                            #bar_chart.render_in_browser()
-                    elif ChartType == 2:
-                        print("you have chosen Line Chart")
-                        print("Select the Time Series of the chart you want to Generate")
-                        print("-----------------------")
-                        print("1. Daily")
-                        print("2. Weekly")
-                        print("3. Monthly")
-                        print("4. Intraday")
-                        TimeSeries = int(input("Enter time series option (1, 2, 3, 4):"))
-                        if TimeSeries != 1 and TimeSeries != 2 and TimeSeries != 3 and TimeSeries != 4:
-                            print("Error Data Type Invalid")
-                        else:
-                            URL = 'https://www.alphavantage.co/query?'+ Function[TimeSeries-1] +'&date=2022-06-09'+ Symbol +'&apikey=7CKUYMD19R6Q9LKW'
-                            print(URL)
-                            StartDate = input("Enter the start date (YYYY-MM-DD):")
-                            EndDate = input("Enter the end date (YYYY-MM-DD):")
-                            r = requests.get(URL)
-                            data = r.json()
-                            print(data)
-                            #line_chart = pygal.Line
-                            #line_chart.add(str(data))
-                            #line_chart.render_in_browser()
-                    else:
-                        print("Error Data Type Invalid")        
-                        StockData = input("Would you like to view more stock data? Press 'y' to continue or 'n' to exit: ")
-            except:
-                print("Error Data Type Invalid")    
-StockMod()
+            StockSymbol = input("Enter the stock symbol you are looking for: ")
+            if StockSymbol == "":
+                print("Error Data Type Invalid")
+            else:
+               Symbol = '&symbol='+ StockSymbol
+               print("Your Symbol was" + Symbol)
+               ChartFunc()          
+
+def ChartFunc():
+    global ChartChoice
+    global ChartType
+            
+    while ChartChoice != 1 and ChartChoice != 2:
+        print("Chart Types")
+        print("-----------------------")
+        print("1. Bar")
+        print("2. Line")
+        ChartChoice = int(input("Enter the chart type you want (1, 2):"))
+        if ChartChoice == 1 or ChartChoice == 2: 
+            print("Chart Type selected was " + ChartType[ChartChoice-1])
+            TimeFunc()
+        else:
+            print("Chart Types")
+            print("-----------------------")
+            print("1. Bar")
+            print("2. Line")
+            print("Data Error Please Select Either '1' or '2' ")
+        
+def TimeFunc(): 
+    global TimeSeries  
+    while TimeSeries != 1 and TimeSeries != 2 and TimeSeries != 3 and TimeSeries != 4:
+        try:
+            print("Select the Time Series of the chart you want to Generate")
+            print("-----------------------")
+            print("1. Daily")
+            print("2. Weekly")
+            print("3. Monthly")
+            print("4. Intraday")     
+            TimeSeries = int(input("Enter time series option (1, 2, 3, 4):"))
+            if TimeSeries == 1:
+                URL = 'https://www.alphavantage.co/query?'+ Function[TimeSeries-1] + Symbol +'&apikey=7CKUYMD19R6Q9LKW'
+                print(URL)
+                StartDate = input("Enter the start date (YYYY-MM-DD):")
+                EndDate = input("Enter the end date (YYYY-MM-DD):")
+                r = requests.get(URL)
+                data = r.json()
+                print(data)
+                
+            elif TimeSeries == 2:
+                URL = 'https://www.alphavantage.co/query?'+ Function[TimeSeries-1] + Symbol +'&apikey=7CKUYMD19R6Q9LKW'
+                print(URL)
+                StartDate = input("Enter the start date (YYYY-MM-DD):")
+                EndDate = input("Enter the end date (YYYY-MM-DD):")
+                r = requests.get(URL)
+                data = r.json()
+                print(data)
+            
+            elif TimeSeries == 3:
+                URL = 'https://www.alphavantage.co/query?'+ Function[TimeSeries-1] + Symbol +'&apikey=7CKUYMD19R6Q9LKW'
+                print(URL)
+                StartDate = input("Enter the start date (YYYY-MM-DD):")
+                EndDate = input("Enter the end date (YYYY-MM-DD):")
+                r = requests.get(URL)
+                data = r.json()
+                print(data)
+                
+            elif TimeSeries == 4:
+                URL = 'https://www.alphavantage.co/query?'+ Function[TimeSeries-1] +'&interval=30min'+ Symbol +'&apikey=7CKUYMD19R6Q9LKW'
+                print(URL)
+                StartDate = input("Enter the start date (YYYY-MM-DD):")
+                EndDate = input("Enter the end date (YYYY-MM-DD):")
+                r = requests.get(URL)
+                data = r.json()
+                print(data)
+        except:
+            print("Select the Time Series of the chart you want to Generate")
+            print("-----------------------")
+            print("1. Daily")
+            print("2. Weekly")
+            print("3. Monthly")
+            print("4. Intraday") 
+            print("Data Error Please Select Either '1','2','3', or '4' ")
+    global StockData
+    StockData = input("Would you like to view more stock data? Press 'y' to continue or 'n' to exit: ") 
+StockFunc()
