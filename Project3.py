40647e13e356ca124a691196836cf2d4cb3aa260
JusticeGtrrz <111757662+JusticeGtrrz@users.noreply.github.com>
2022-10-26 11:10:58 -0500

    Update Project3.py

--- Project3.py
+++ Project3.py
@@ -44,54 +44,55 @@ def StockFunc():
                         print("1. Bar")
                         print("2. Line")
                         print("Data Error Please Select Either '1' or '2': ")
-                while TimeSeries != 1 and TimeSeries != 2 and TimeSeries != 3 and TimeSeries != 4:
-                    try:
-                        print("Select the Time Series of the chart you want to Generate")
-                        print("-----------------------")
-                        print("1. Daily")
-                        print("2. Weekly")
-                        print("3. Monthly")
-                        print("4. Intraday")     
-                        TimeSeries = int(input("Enter time series option (1, 2, 3, 4): "))
-                        if TimeSeries == 1:
-                            URL = 'https://www.alphavantage.co/query?'+ Function[TimeSeries-1] + Symbol +'&apikey=7CKUYMD19R6Q9LKW'
-                            print(URL)
-                            r = requests.get(URL)
-                            data = r.json()
-                            print(data)
-                            
-                        elif TimeSeries == 2:
-                            URL = 'https://www.alphavantage.co/query?'+ Function[TimeSeries-1] + Symbol +'&apikey=7CKUYMD19R6Q9LKW'
-                            print(URL)
-                            r = requests.get(URL)
-                            data = r.json()
-                            print(data)
+            while TimeSeries != 1 and TimeSeries != 2 and TimeSeries != 3 and TimeSeries != 4:
+                try:
+                    print("Select the Time Series of the chart you want to Generate")
+                    print("-----------------------")
+                    print("1. Daily")
+                    print("2. Weekly")
+                    print("3. Monthly")
+                    print("4. Intraday")     
+                    TimeSeries = int(input("Enter time series option (1, 2, 3, 4): "))
+                    if TimeSeries == 1:
+                        URL = 'https://www.alphavantage.co/query?'+ Function[TimeSeries-1] + Symbol +'&apikey=7CKUYMD19R6Q9LKW'
+                        print(URL)
+                        r = requests.get(URL)
+                        data = r.json()
+                        print(data)
                         
-                        elif TimeSeries == 3:
-                            URL = 'https://www.alphavantage.co/query?'+ Function[TimeSeries-1] + Symbol +'&apikey=7CKUYMD19R6Q9LKW'
-                            print(URL)
-                            r = requests.get(URL)
-                            data = r.json()
-                            print(data)
-                            
-                        elif TimeSeries == 4:
-                            URL = 'https://www.alphavantage.co/query?'+ Function[TimeSeries-1] +'&interval=30min'+ Symbol +'&apikey=7CKUYMD19R6Q9LKW'
-                            print(URL)
-                            r = requests.get(URL)
-                            data = r.json()
-                            print(data)
-                    except:
-                        print("Select the Time Series of the chart you want to Generate")
-                        print("-----------------------")
-                        print("1. Daily")
-                        print("2. Weekly")
-                        print("3. Monthly")
-                        print("4. Intraday") 
-                        print("Data Error Please Select Either '1','2','3', or '4': ")
+                    elif TimeSeries == 2:
+                        URL = 'https://www.alphavantage.co/query?'+ Function[TimeSeries-1] + Symbol +'&apikey=7CKUYMD19R6Q9LKW'
+                        print(URL)
+                        r = requests.get(URL)
+                        data = r.json()
+                        print(data)
+                    
+                    elif TimeSeries == 3:
+                        URL = 'https://www.alphavantage.co/query?'+ Function[TimeSeries-1] +'&interval=30min'+ Symbol +'&apikey=7CKUYMD19R6Q9LKW'
+                        print(URL)
+                        r = requests.get(URL)
+                        data = r.json()
+                        print(data)
+                        
+                    elif TimeSeries == 4:
+                        URL = 'https://www.alphavantage.co/query?'+ Function[TimeSeries-1] +'&interval=30min'+ Symbol +'&apikey=7CKUYMD19R6Q9LKW'
+                        print(URL)
+                        r = requests.get(URL)
+                        data = r.json()
+                        print(data)
+                except:
+                    print("Select the Time Series of the chart you want to Generate")
+                    print("-----------------------")
+                    print("1. Daily")
+                    print("2. Weekly")
+                    print("3. Monthly")
+                    print("4. Intraday") 
+                    print("Data Error Please Select Either '1','2','3', or '4': ")
+
+            StockData = input("Would you like to view more stock data? Press 'y' to continue or 'n' to exit: ")
+            if StockData !="y" and StockData !="n":
+                print("Error Data Type Invalid. Please enter 'y' or 'n': ")
                 StockData = input("Would you like to view more stock data? Press 'y' to continue or 'n' to exit: ")
-                if StockData !="y" and StockData !="n":
-                    print("Error Data Type Invalid. Please enter 'y' or 'n': ")
-                    StockData = input("Would you like to view more stock data? Press 'y' to continue or 'n' to exit: ")
             ChartChoice = 0
             StockSymbol = ""
             Symbol = ""
