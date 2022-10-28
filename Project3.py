34c3f443af230f8ff568c15940aeec35922ad0dc
JusticeGtrrz <111757662+JusticeGtrrz@users.noreply.github.com>
2022-10-27 19:38:55 -0500

    Update Project3.py

--- Project3.py
+++ Project3.py
@@ -32,7 +32,6 @@ def PopChart():
         Input=(input('Please enter Start Date: '))
         if (Input in list(StepData)):
             Index = list(StepData).index(Input)
-            print("Element Exists")
         else:
             while (Input in list(StepData)) == False:
                 dateArray = Input.split('-')
@@ -46,7 +45,6 @@ def PopChart():
         SecondInput=(input('Please enter End Date: '))
         if (SecondInput in list(StepData)):
             Index2 = list(StepData).index(SecondInput)
-            print("Element Exists")
         else:
             while (SecondInput in list(StepData)) == False:
                 dateArray = SecondInput.split('-')
@@ -110,34 +108,6 @@ def LineChart():
     line_chart.add('close', Close)
     line_chart.render_in_browser()
 
-
-def getStartDate(data, startDate):
-    try:
-        return list(data).index(startDate)
-    except:
-        dateArray = startDate.split('-')
-        dateNum = int(dateArray[2])
-        if dateNum == 31:
-            dateNum = 1
-        else:
-            dateNum = dateNum + 1
-        getStartDate(data, dateArray[0] + '-' + dateArray[1] + '-' + str(dateNum))
-
-def getEndDate(data, endDate):
-    try:
-        return list(data).index(endDate)
-    except:
-        dateArray = endDate.split('-')
-        dateNum = int(dateArray[2])
-        if dateNum == 1:
-            dateNum = 31
-        else:
-            dateNum = dateNum - 1
-        getEndDate(data, dateArray[0] + '-' + dateArray[1] + '-' + str(dateNum))
-        
-#How to use:
-# Index = list(StepData).index(getStartDate(StepData, input('Please enter Start Date: ')))
-# Index2 = list(StepData).index(getEndDate(StepData, input('Please enter End Date: ')))
 def StockFunc():
     
     global ChartChoice
